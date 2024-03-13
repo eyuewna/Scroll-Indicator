0bb85e62845c8a291567778a33d2974e8e2eff72
Euna Angeles <202210630@fit.edu.ph>
2024-03-13 21:31:26 +0800

    Update script.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,25 @@
+const scrollIndicator = document.getElementById('scroll-indicator');
+const totalScrollHeight = document.body.scrollHeight - window.innerHeight;
+const videoSource = document.getElementById('video-source');
+const videoBackground = document.getElementById('video-background');
+const audioPlayer = new Audio('../audio/goddess.mp3');
 
+function updateScrollIndicator() {
+    const scrollPercent = (window.scrollY / totalScrollHeight) * 100;
+    scrollIndicator.style.width = `${scrollPercent}%`;
+}
+
+function resetVideo() {
+    videoBackground.play();
+
+    audioPlayer.play();
+}
+
+window.addEventListener('scroll', updateScrollIndicator);
+
+const photocards = document.querySelectorAll('.photocard');
+
+photocards.forEach(photocard => {
+    photocard.addEventListener('click', resetVideo);
+    photocard.addEventListener('mouseout', resetVideo);
+});
